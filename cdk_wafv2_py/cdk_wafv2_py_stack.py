9c5a6b2a2f28978dc0d8ed08d42ec3c15ff9f07b
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2021-06-23 20:12:53 +0000

    first commit

--- cdk_wafv2_py/cdk_wafv2_py_stack.py
+++ cdk_wafv2_py/cdk_wafv2_py_stack.py
@@ -1,5 +1,7 @@
 from aws_cdk import core as cdk
 
+import aws_cdk.aws_wafv2 as waf
+
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
@@ -13,3 +15,68 @@ class CdkWafv2PyStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        
+        acl = waf.CfnWebACL
+        
+        # BlockQueriesContainingSubString via query_string
+        
+        custom_rule = acl.RuleProperty(
+            name='BlockQueriesContainingSubString',
+            priority=1,
+            action=acl.RuleActionProperty(block={}),
+            statement=acl.StatementProperty(
+                byte_match_statement=acl.ByteMatchStatementProperty(
+                    search_string='blockme',
+                    field_to_match=acl.FieldToMatchProperty(single_header={'Name':'Referer'}),
+                    positional_constraint='EXACTLY',
+                    text_transformations=[
+                        acl.TextTransformationProperty(priority=0, type='NONE'),
+                    ],
+                )
+            ),
+            visibility_config=acl.VisibilityConfigProperty(
+                cloud_watch_metrics_enabled=True,
+                sampled_requests_enabled=True,
+                metric_name='custom_rule',
+            )
+        )
+        
+        custom_rule2 = acl.RuleProperty(
+            name='BlockQueriesContainingSubString2',
+            priority=1,
+            action=acl.RuleActionProperty(block={}),
+            statement=acl.StatementProperty(
+                byte_match_statement=acl.ByteMatchStatementProperty(
+                    search_string='blockme',
+                    field_to_match=acl.FieldToMatchProperty(single_header={'Name':'User-Agent'}),
+                    positional_constraint='EXACTLY',
+                    text_transformations=[
+                        acl.TextTransformationProperty(priority=0, type='NONE'),
+                    ],
+                )
+            ),
+            visibility_config=acl.VisibilityConfigProperty(
+                cloud_watch_metrics_enabled=True,
+                sampled_requests_enabled=True,
+                metric_name='custom_rule2',
+            )
+        )
+        
+        
+        
+        
+        web_acl = waf.CfnWebACL(
+            self, 'WebACL',
+            default_action=acl.DefaultActionProperty(allow={}),
+            scope='REGIONAL',  # or use CLOUDFRONT if protecting a distribution
+            visibility_config=acl.VisibilityConfigProperty(
+                cloud_watch_metrics_enabled=True,
+                metric_name='webACL',
+                sampled_requests_enabled=True
+            ),
+            name=f'test-acl',
+            rules=[
+                custom_rule,
+                custom_rule2,
+            ]
+        )
